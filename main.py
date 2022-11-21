1d1af6e7b068bd87b2e2e45f413f8364c8f04274
Arsen <xakars02@gmail.com>
2022-11-22 00:01:26 +0600

    rename file, fix spaces

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+import curses
+
+
+def draw(canvas):
+    row, column = (10, 25)
+    curses.curs_set(False)
+    canvas.border()
+    while True:
+        canvas.addstr(row, column, '*', curses.A_DIM)
+        canvas.refresh()
+        time.sleep(2)
+        canvas.addstr(row, column, '*')
+        canvas.refresh()
+        time.sleep(0.3)
+        canvas.addstr(row, column, '*', curses.A_BOLD)
+        canvas.refresh()
+        time.sleep(0.5)
+        canvas.addstr(row, column, '*')
+        canvas.refresh()
+        time.sleep(0.3)
+
+
+if __name__ == '__main__':
+	curses.update_lines_cols()
+	curses.wrapper(draw)
